b3936aedb88655f61ace007eed8c72fa19b7ecc1
Stephan Kunne <stephan.kunne@gmail.com>
2019-08-14 11:45:01 +0200

    add executable droptables to reset the psql database

---
+++ small_tests/main_droptables.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>			/* display progress */
+#include <libpq-fe.h>       /* PostgreSQL */
+
+#define DROPTABLE	"DROP TABLE IF EXISTS "
+#define CASCADE		" CASCADE;"
+
+bool printStatus(PGresult *result)
+{
+	ExecStatusType status = PQresultStatus(result);
+
+	bool empty_query = (status == PGRES_EMPTY_QUERY);
+	bool bad_response = (status == PGRES_BAD_RESPONSE);
+	bool fatal_error = (status == PGRES_FATAL_ERROR);
+	bool everything_ok = !(empty_query || bad_response || fatal_error);
+
+	std::cout << "Status: " << PQresStatus(status) << std::endl;
+	if (!everything_ok)
+		std::cout << PQresultErrorMessage(result);
+
+	return everything_ok;
+}
+
+int main(void)
+{
+	std::string tablenames[] = {"helloworld", "files", "linkselectors", "replicas", "sites", "storageelements", "transfers"};
+	PGconn *conn = PQconnectdb("user=admin host=dbod-skunne-testing.cern.ch port=6601 dbname=postgres");
+	for (std::string name : tablenames)
+	{
+    	const std::string str = DROPTABLE + name + CASCADE;
+    	std::cout << std::endl << str << std::endl;
+    	PGresult *result = PQexec(conn, str.c_str());
+    	printStatus(result);
+	}
+	PQfinish(conn);
+}
